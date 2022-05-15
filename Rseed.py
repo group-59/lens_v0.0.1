2fd4bcb96d084b01f5998957c11a0d592638b1ab
nine <61563962+group-59@users.noreply.github.com>
2022-05-14 22:30:50 -0500

    Create Rseed.py

---
+++ Rseed.py
@@ -0,0 +1,12 @@
+## created by room 109
+## reads the seedgenTX.txt file
+x=[]
+f=open("drive://path/seedgenTX.txt", "r")
+for rlist in f:
+  A = rlist
+  x.append(A)
+last_int = x[4]
+## check if last_int is the last thing in seedgenTX.txt in there
+
+if last_int is true:
+  
